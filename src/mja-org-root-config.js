5b2631931527649e1a8fcecacd44185437ce7ad3
MJ Abadilla <mjmaix@gmail.com>
2022-10-09 22:48:55 +0800

    Mount apps to specific location

--- src/mja-org-root-config.js
+++ src/mja-org-root-config.js
@@ -4,19 +4,22 @@ import * as isActive from "./activity-functions";
 registerApplication(
   "@mja-org/single-spa-demo-nav",
   () => System.import("@mja-org/single-spa-demo-nav"),
-  isActive.nav
+  isActive.nav,
+  { domElement: document.getElementById("nav-container") }
 );
 
 registerApplication(
   "@mja-org/single-spa-demo-page-1",
   () => System.import("@mja-org/single-spa-demo-page-1"),
-  isActive.page1
+  isActive.page1,
+  { domElement: document.getElementById("page-1-container") }
 );
 
 registerApplication(
   "@mja-org/single-spa-demo-page-2",
   () => System.import("@mja-org/single-spa-demo-page-2"),
-  isActive.page2
+  isActive.page2,
+  { domElement: document.getElementById("page-2-container") }
 );
 
 start({
